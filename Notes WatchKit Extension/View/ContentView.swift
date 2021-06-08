f46976a13246d32cb27e9d75051aed903b371d4c
Gaurav Bhasin <bhasingaurav@gmail.com>
2021-06-08 19:20:09 -0400

    Refreshed!

--- Notes WatchKit Extension/View/ContentView.swift
+++ Notes WatchKit Extension/View/ContentView.swift
@@ -8,52 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    // MARK: - PROPERTIES
-    
-    @State private var notes = [Note]()
-    @State private var text = ""
-    @AppStorage("lineCount") var lineCount = 1
-    
-    // MARK: - FUNCTIONS
-    
-    func delete(offsets: IndexSet) {
-        withAnimation {
-            notes.remove(atOffsets: offsets)
-            save()
-        }
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func save() {
-        do {
-            let data = try JSONEncoder().encode(notes)
-            let url = getDocumentsDirectory().appendingPathComponent("notes")
-            try data.write(to: url)
-        } catch {
-            print(error)
-        }
-    }
-    
-    // MARK: - BODY
-    
     var body: some View {
-        VStack {
-            HStack {
-                TextField("Add new note", text: $text)
-                Image(systemName: "plus.circle")
-                    .font(.largeTitle)
-                    .foregroundColor(.yellow)
-            }
-            List {
-                ForEach(notes) { note in
-                    Text("Note")
-                }
-            }
-        }
+        Text("Hello, World!")
     }
 }
 
